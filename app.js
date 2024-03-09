6531a6e23d94f8ab3c2246bd5437798ecfc59a11
umair-aligtk <156018837+umair-aligtk@users.noreply.github.com>
2024-03-09 11:17:07 +0500

    web

---
+++ app.js
@@ -0,0 +1,248 @@
+//  var city = prompt("Enter your city")
+
+// if(city == "karachi"){
+//     document.write("Welcometo the city of light" )
+// }
+// else(
+//     document.write("Please Enter city name karachi" )
+// )
+
+
+// var gender = prompt("Enter your gender")
+
+// if(gender === "Male"){
+//     document.write("Good morning sir" )
+// }
+
+
+// if(gender==="female"){
+//     document.write("Good morning Ma'am" )
+// }
+
+// var signal = prompt("Enter a signal color")
+
+// if(signal== "red"){
+//     document.write("Must stop")
+
+// }
+
+
+// if(signal== "yellow"){
+//     document.write("Ready to move")
+
+// }
+
+
+// if(signal== "green"){
+//     document.write("Move now")
+
+// }
+
+// var fuel = prompt("Remainig fuel in car" )
+// if(fuel<="1"){
+// document.write("“Please refill the fuel in your car”")
+// }
+
+// var a = 4;
+// if(++a===5){
+//    alert("given condition for variable a is true");
+// }
+
+
+//  var b = 82;
+//  if(b++===83){
+//    alert("given condition for variable b is true");
+//  }
+//  document.write(b)
+
+// var c =12;
+// if(c++==13){
+// alert("Condition 1 is true")}
+// //  document.write(c)
+// if(c===13){
+//     alert("Condition 2 is true")
+// }
+// if(c++<14){
+//     alert("Condition 3 is true")
+// }
+// if(c===14){
+//     alert("Condition 4 is true")
+// }
+
+// var material_cost =20000;
+// var labour_cost =2000;
+// var total_cost = material_cost + labour_cost;
+// if(total_cost === material_cost + labour_cost){
+//     alert("The cost equals")
+// }
+
+// if(true){
+//     alert("true")
+//}
+// if(false){
+//     alert("false")
+// }
+
+// if("car"< "cat"){
+//     alert("Car is smaller then cat")
+// }
+
+
+// var EngMarks = +prompt("Marks in English?"),
+//     SciMarks = +prompt("Marks in Science?"),
+//     MathMarks = +prompt("Marks in Math?"),
+//     TotalMarks = +prompt("Total Marks"),
+//     Percentage = ((EngMarks + SciMarks + MathMarks) / TotalMarks) * 100, Grade, Remarks;
+
+
+// if (Percentage >= 80) {
+//     Grade = "A+";
+//     Remarks = "Excellent";
+// }
+// else if (Percentage >= 70) {
+//     Grade = "A";
+//     Remarks = "Good";
+// }
+// else if (Percentage >= 60) {
+//     Grade = "B";
+//     Remarks = "You need to Improve";
+// }
+// else if (Percentage < 60) {
+//     Grade = "Fail";
+//     Remarks = "Sorry";
+// }
+// else {
+//     Grade = "Invalid";
+//     Remarks = "Invalid";
+// }
+// document.write("<h1>Marks Sheet</h1></br>Total Marks : " + TotalMarks + "</br>Marks Obtained : " + (EngMarks + SciMarks + MathMarks) + "</br>Percentage : " + Percentage + "%</br>Grade : " + Grade + "</br>Remarks : " + Remarks);
+
+
+// Qno:7
+
+// var secret = +prompt("Enter correct number between 1 to 100")
+// if(secret == "7"){
+//     document.write("“Bingo! Correct answer”.")
+// }
+// else(
+// document.write("“Close enough to the correct answer”.")
+// )
+
+// Qno:8 
+
+// var num = +prompt("Enter your number")
+// if(num%3===0){
+// document.write("It is devisible by 3")
+// }
+// else{
+//     document.write("Not divisable by 3")
+// }
+
+
+// Qno: 9
+// var num = +prompt("Enter a number")
+// if (num % 2 === 0) {
+//     document.write("It is Even")
+// }
+// else {
+//     document.write("It is odd number")
+// }
+
+
+// var temp = +prompt("Enter a temperature")
+// if (temp >= 40) {
+//     document.write("“It is too hot outside.”")
+// }
+
+// else if (temp > 30) {
+//     document.write("“The Weather today is Normal.”")
+// }
+
+// else if (temp > 20) {
+//     document.write("“Today’s Weather is cool.”")
+// }
+
+// else {
+//     document.write("“OMG! Today’s weather is so Cool.”")}
+
+
+// var num1=+prompt("Enter a first number")
+// var opt = prompt("operator")
+// var num2 =+prompt("Enter a second number")
+
+
+// if(opt=="+"){
+//     alert(num1 + num2)
+// }
+// else if (opt== "-"){
+//     alert(num1-num2)
+// }
+
+// else if (opt== "*"){
+//     alert(num1*num2)
+// }
+
+// else if (opt=="/"){
+//     alert( num2num1)
+// }
+// else{
+//     alert("Please enter a number")
+// }
+
+
+//complete
+
+
+//Chapter 11-12
+
+Qno: 2
+
+
+// var num1 =+ prompt("Enter first value in numbers" )
+// var num2 =+ prompt("Enter second value in numbers" )
+// if (num1 > num2){
+//     alert(num1+ "is grater then" +num2)
+// }
+
+// else if (num2 > num1 ){
+//     alert(num2+ "is Grater then"+num1)
+// }
+
+// else if (num1=== num2 ){
+//     alert("Entered both digits are equal")
+// }
+// else(
+//     alert("In correct number")
+// )
+
+Qno: 3
+
+// var number = prompt("Enter a number")
+
+// if (number > 0) {
+//     alert("number is positive")
+// }
+
+// else if (number == 0) {
+//     alert("number is zero")
+// }
+
+// else if (number < 0) {
+//     alert("number is negative")
+// }
+// else {
+//     alert("Incorrect")
+// }
+
+// Q no:5
+// var password = "umair123"
+// var passwordinput=prompt("Enter a password")
+// if( passwordinput != password ){
+//     alert("the given password is not correct" )
+// }
+// else if(password=password){
+//     alert("The given password is correct")
+// }
+// else{
+//     alert("Incorrect password")
+// }
